28e5db213dd488316a12444a6b739f116cf4c402
Santiago Guerrero Martinez <47578270+CodiPop@users.noreply.github.com>
2023-06-02 14:35:31 -0500

    Added cors

--- GuardiansOfTheGlobe_graphQLApi/GuardiansOfTheGlobe_graphQLApi/Program.cs
+++ GuardiansOfTheGlobe_graphQLApi/GuardiansOfTheGlobe_graphQLApi/Program.cs
@@ -8,6 +8,7 @@ using Microsoft.EntityFrameworkCore;
 using System;
 
 var builder = WebApplication.CreateBuilder(args);
+builder.Services.AddCors();
 builder.Services.AddDbContext<AppDbContext>(option => option.UseSqlServer(builder.Configuration.GetConnectionString("AppConnection")));
 //builder.Services.AddGraphQLServer().AddQueryType<Qheroe>().AddMutationType<Mutation>();
 builder.Services.AddGraphQLServer()
@@ -32,7 +33,12 @@ builder.Services.AddGraphQLServer()
 var app = builder.Build();
 
 //builder.Services.AddControllers();
-
+app.UseCors(builder =>
+{
+    builder.AllowAnyOrigin()
+        .AllowAnyMethod()
+        .AllowAnyHeader();
+});
 app.MapGraphQL();
 
 app.MapGet("/", () => "Hello World!");
